392b95fe0036985d2b4be6804260477915ad4894
Bruno Brustoloni e Oliveira <bbrustol@gmail.com>
2018-12-14 20:24:00 -0200

    update item and Adapter

--- app/src/main/java/com/bbrustol/cmindtest/presentation/news/NewsAdapter.kt
+++ app/src/main/java/com/bbrustol/cmindtest/presentation/news/NewsAdapter.kt
@@ -1,24 +1,25 @@
 package com.bbrustol.cmindtest.presentation.news
 
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.SimpleItemAnimator
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.bbrustol.cmindtest.R
-import com.bbrustol.cmindtest.data.model.SourcesModel
 import io.reactivex.Observable
 import io.reactivex.subjects.PublishSubject
+import com.bbrustol.cmindtest.R
+import com.bbrustol.cmindtest.data.model.SourcesModel
 import kotlinx.android.synthetic.main.item_source_news.view.*
 
 class NewsAdapter: RecyclerView.Adapter<ViewHolder>() {
 
-    var mSourcesList: ArrayList<SourcesModel> = arrayListOf()
+    private var mSourcesList: ArrayList<SourcesModel> = arrayListOf()
     private lateinit var mRecyclerView: RecyclerView
 
     private val clickWebviewButtonSubject = PublishSubject.create<String>()
     val clickWebviewButtonEvent: Observable<String> = clickWebviewButtonSubject
 
-    val clickItemSubject = PublishSubject.create<String>()
+    private val clickItemSubject = PublishSubject.create<String>()
     val clickItemEvent: Observable<String> = clickItemSubject
 
     override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
@@ -32,13 +33,10 @@ class NewsAdapter: RecyclerView.Adapter<ViewHolder>() {
 
     // Inflates the item views
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_source_news, parent, false), mSourcesList)
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_source_news, parent, false))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
-//        val viewHolder = holder
-//        viewHolder.configItem()
 
         val source = mSourcesList[position]
 
@@ -54,37 +52,16 @@ class NewsAdapter: RecyclerView.Adapter<ViewHolder>() {
             }
         }
 
-//        (mRecyclerView.itemAnimator as SimpleItemAnimator).supportsChangeAnimations = false
+        (mRecyclerView.itemAnimator as SimpleItemAnimator).supportsChangeAnimations = false
     }
-
-
 
 
     fun updateData(
         newData: ArrayList<SourcesModel>
     ) {
         mSourcesList = newData
-        notifyItemRangeInserted(0, mSourcesList.size);
+        notifyDataSetChanged()
     }
 }
 
-
-class ViewHolder (
-    itemView: View,
-    var list: ArrayList<SourcesModel>
-) : RecyclerView.ViewHolder(itemView) {
-
-    fun configItem() {
-        itemView.apply {
-            source_news_item_tv_title.text = list.get(adapterPosition).name
-            source_news_item_tv_description.text = list.get(adapterPosition).description
-//            source_news_item_website.setOnClickListener {
-//                clickWebviewButtonSubject.onNext(list.get(adapterPosition).url)
-//            }
-//
-//            setOnClickListener {
-//                clickItemSubject.onNext(list.get(adapterPosition).id)
-//            }
-        }
-    }
-}
+class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView)
